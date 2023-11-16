26a882e5c9905efd940dd6c6992d683781b7f23b
asiyeyildirim <asiyeeyildirim71@gmail.com>
2023-11-16 15:03:46 +0300

    Tasarım oluşturuldu

--- app/src/main/java/com/kotlinegitim/superkahramanproject/MainActivity.kt
+++ app/src/main/java/com/kotlinegitim/superkahramanproject/MainActivity.kt
@@ -2,10 +2,18 @@ package com.kotlinegitim.superkahramanproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun superKahramanYap(view: View){
+
+        
+
     }
 }
