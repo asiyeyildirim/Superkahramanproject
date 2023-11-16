ab27314a2efba9b7841f42463781b9b9e6783edf
asiyeyildirim <asiyeeyildirim71@gmail.com>
2023-11-16 20:43:25 +0300

    kahraman oluştur buton
    butonu aktif edip girilen değerleri ekrana yazdırma işlemi

--- app/src/main/java/com/kotlinegitim/superkahramanproject/MainActivity.kt
+++ app/src/main/java/com/kotlinegitim/superkahramanproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kotlinegitim.superkahramanproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,11 @@ class MainActivity : AppCompatActivity() {
 
     fun superKahramanYap(view: View){
 
-        
+        val name = nameText.text.toString()
+        val age = ageText.text.toString()
+        val job = jobText.text.toString()
+
+        sonucText.text = "İsim: ${name}  Yaş: ${age} Meslek:${job}"
 
     }
 }
